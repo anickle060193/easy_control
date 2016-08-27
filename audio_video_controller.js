b44aa9215561b453f958d41d11b7c36a26a809aa
Adam Nickle <anickle060193@gmail.com>
2016-08-27 17:07:35 -0500

    Only request name if a video or audio element is found.
    Relates to #16.

--- audio_video_controller.js
+++ audio_video_controller.js
@@ -10,22 +10,28 @@ AudioVideoController.prototype.constructor = AudioVideoController;
 
 $( function()
 {
-    chrome.runtime.sendMessage( null, new Message( Message.types.to_background.NAME_REQUEST ), function( name )
-    {
-        $( 'video' ).each( function( i, video )
-        {
-            var videoName = 'Video_' + i + '_' + name;
-            console.log( 'Video found: ' + videoName );
-            var videoController = new AudioVideoController( video, videoName );
-            videoController.startPolling();
-        } );
+    var videos = $( 'video' );
+    var audios = $( 'audio' );
 
-        $( 'audio' ).each( function( i, audio )
+    if( videos.length !== 0 || audios.length !== 0 )
+    {
+        chrome.runtime.sendMessage( null, new Message( Message.types.to_background.NAME_REQUEST ), function( name )
         {
-            var audioName = 'Audio_' + i + '_' + name;
-            console.log( 'Audio found: ' + audioName );
-            var audioController = new AudioVideoController( audio, audioName );
-            audioController.startPolling();
+            videos.each( function( i, video )
+            {
+                var videoName = 'Video_' + i + '_' + name;
+                console.log( 'Video found: ' + videoName );
+                var videoController = new AudioVideoController( video, videoName );
+                videoController.startPolling();
+            } );
+
+            audios.each( function( i, audio )
+            {
+                var audioName = 'Audio_' + i + '_' + name;
+                console.log( 'Audio found: ' + audioName );
+                var audioController = new AudioVideoController( audio, audioName );
+                audioController.startPolling();
+            } );
         } );
-    } );
+    }
 } );
