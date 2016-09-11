002bd118b8c7145ef90a5840f5bd11aea4cfb6f5
Adam Nickle <anickle060193@gmail.com>
2016-09-11 03:38:35 -0500

    Made updating change log easier by generating repeated content.

--- src/change_log/change_log.js
+++ src/change_log/change_log.js
@@ -1,1 +1,26 @@
-$( 'body' ).scrollspy( { target : '#versions-sidebar', offset : 50 } );
+$( function()
+{
+    $( 'body' ).scrollspy( { target : '#versions-sidebar', offset : 60 } );
+
+    $( 'a[data-issue]' ).each( function()
+    {
+        this.href = "https://github.com/anickle060193/easy_control/issues/" + this.dataset.issue;
+        $( this ).text( "[Issue #" + this.dataset.issue + "]" );
+    } );
+
+    $( 'span[data-type]' ).each( function()
+    {
+        if( this.dataset.type === 'enhancement' )
+        {
+            $( this ).addClass( 'label label-info' ).text( 'Enhancement' );
+        }
+        else if( this.dataset.type === 'bug' )
+        {
+            $( this ).addClass( 'label label-danger' ).text( 'Bug Fix' );
+        }
+        else if( this.dataset.type === 'feature' )
+        {
+            $( this ).addClass( 'label label-success' ).text( 'New Feature' );
+        }
+    } );
+} );
