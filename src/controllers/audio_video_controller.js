907d784be7c8e46568f0e8ec5981962b8b4ac6c1
Adam Nickle <anickle060193@gmail.com>
2017-06-04 14:45:55 -0500

    Dynamically determine icon color for generic media controller.
    Closes #117

--- src/controllers/audio_video_controller.js
+++ src/controllers/audio_video_controller.js
@@ -1,10 +1,10 @@
 class AudioVideoController extends MediaController
 {
-    constructor( media )
+    constructor( media, color )
     {
         super( 'GenericAudioVideo', media );
 
-        this.color = Controller.settings[ Settings.ControllerColors.GenericAudioVideo ];
+        this.color = color || Controller.settings[ Settings.ControllerColors.GenericAudioVideo ];
         this.hostname = null;
 
         this.initialize();
@@ -21,6 +21,18 @@ $( function()
 {
     if( Controller.settings[ Settings.ControllersEnabled.GenericAudioVideo ] )
     {
+        var color = Controller.settings[ Settings.ControllerColors.GenericAudioVideo ];
+        var metaThemeColor = $( 'meta[name="theme-color"][content][content!=""]:first' );
+        var metaTileColor = $( 'meta[name="msapplication-TileColor"][content][content!=""]:first' );
+        if( metaThemeColor.length > 0 )
+        {
+            color = metaThemeColor.attr( 'content' );
+        }
+        else if( metaTileColor.length > 0 )
+        {
+            color = metaTileColor.attr( 'content' );
+        }
+
         var url = window.location.href;
         var blacklist = Controller.settings[ Settings.SiteBlacklist ];
         for( var i = 0; i < blacklist.length; i++ )
@@ -49,7 +61,7 @@ $( function()
             }
 
             mediaCounter++;
-            return new AudioVideoController( media );
+            return new AudioVideoController( media, color );
         } );
     }
 } );
