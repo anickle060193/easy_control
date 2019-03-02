37ced572b38bcbf0f82549a21a3696198bdefd00
Adam Nickle <anickle060193@gmail.com>
2019-03-02 12:28:29 -0600

    Improve notification handling.

--- src/common/content.ts
+++ src/common/content.ts
@@ -1,9 +1,9 @@
 export interface ContentInfo
 {
   title: string;
-  caption: string;
-  subcaption: string;
-  image: string | null | undefined;
+  caption?: string | null;
+  subcaption?: string | null;
+  image?: string | null;
   isLiked: boolean;
   isDisliked: boolean;
   link: string;
