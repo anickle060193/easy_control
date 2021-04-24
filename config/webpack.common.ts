15196426194e1ee1887854635734a4ba49366e5a
Adam Nickle <anickle060193@gmail.com>
2021-04-24 01:18:32 -0500

    Initial commit.

---
+++ config/webpack.common.ts
@@ -0,0 +1,66 @@
+import path from 'path';
+import webpack from 'webpack';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const root = path.resolve( __dirname, '..' );
+const build = path.resolve( root, 'build' );
+const src = path.resolve( root, 'src' );
+
+const config: webpack.Configuration = {
+  entry: {
+    content: path.resolve( src, 'content', 'index.ts' ),
+  },
+  output: {
+    path: build,
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx', '.ts', '.tsx', '.json' ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin( {
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+      },
+      eslint: {
+        files: path.resolve( src, '**', '*.{js,jsx,ts,tsx}' ),
+      },
+    } ),
+    new CopyWebpackPlugin( {
+      patterns: [
+        {
+          from: path.resolve( src, 'manifest.json' ),
+          to: build,
+          toType: 'dir',
+          transform: ( content ) => JSON.stringify( {
+            name: process.env[ 'npm_package_displayName' ],
+            description: process.env[ 'npm_package_description' ],
+            version: process.env[ 'npm_package_version' ],
+            ...JSON.parse( content.toString() ),
+          }, null, 2 ),
+        },
+        {
+          from: path.resolve( root, 'assets' ),
+          to: path.resolve( build, 'assets' ),
+        },
+      ],
+    } ),
+  ],
+};
+
+export default config;
---
+++ config/webpack.dev.ts
@@ -0,0 +1,8 @@
+import merge from 'webpack-merge';
+
+import common from './webpack.common';
+
+export default merge( common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+} );
