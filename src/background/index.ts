9439b66d90677a02c0fb9f470cbafc5f132a50a0
Adam Nickle <anickle060193@gmail.com>
2021-05-06 21:47:37 -0500

    Added install version logging.

--- src/background/index.ts
+++ src/background/index.ts
@@ -6,6 +6,24 @@ import { initControllers } from './controllers';
 import { initNotifications } from './notifications';
 import { initControlsPopup } from './controlsPopup';
 
+chrome.runtime.onInstalled.addListener( ( details ) =>
+{
+  const version = chrome.runtime.getManifest().version;
+
+  if( details.reason === 'install' )
+  {
+    console.log( 'Installed:', version );
+  }
+  else if( details.reason === 'update' )
+  {
+    console.log( 'Updated:', details.previousVersion, '->', version );
+  }
+  else
+  {
+    console.log( 'Unknown Install Reason:', details );
+  }
+} );
+
 initContextMenus();
 initCommands();
 initBrowserAction();
