bbf3955c149dda36b20b4e2dfb12993d8aa7c34d
Adam Nickle <anickle060193@gmail.com>
2025-02-15 18:23:07 -0600

    Update changelog.

--- src/changelog/ChangelogPage.tsx
+++ src/changelog/ChangelogPage.tsx
@@ -70,6 +70,10 @@ export const ChangelogPage: React.FC = () =>
           overflowY: 'auto',
         }}
       >
+        <ChangelogVersion version="4.0.4">
+          <ChangelogEntry changeType="bug" text="Fixed Spotify controller." />
+        </ChangelogVersion>
+
         <ChangelogVersion version="4.0.3">
           <ChangelogEntry changeType="bug" text="Prevent background controller from being unloaded while any media controllers are active." />
         </ChangelogVersion>
